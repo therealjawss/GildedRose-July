0f59b9829e334f1d7c4b60087b3a1f3b1f5db2ae
therealjawss <jawss@thecodinghammock.com>
2019-07-20 14:35:56 +0200

    Added separate updater

---
+++ src/GildedRose.Tests/UpdaterShould.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using GildedRose.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class UpdaterShould
+    {
+        [Theory]
+        [MemberData(nameof(ItemsData))]
+        public void CreateAnUpdaterForItems(string name) {
+            var updater = new Updater();
+            updater.Should().NotBeNull();
+        }
+
+        public static IEnumerable<object[]> ItemsData => new List<object[]> {
+            new object[] { TestConstants.BRIE},
+            new object[] { TestConstants.CONJURED},
+            new object[] { TestConstants.DEXTERITY},
+        };
+    }
+}
