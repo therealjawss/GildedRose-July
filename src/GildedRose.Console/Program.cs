f4761c0137fd446eba4bd90ac355799d2c92e4c7
therealjawss <jawss@thecodinghammock.com>
2019-07-22 09:07:38 +0200

    Updated for loop into foreach and extracted method for easier testing

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -2,9 +2,16 @@
 
 namespace GildedRose.Console
 {
-    class Program
+    public class Program
     {
         IList<Item> Items;
+
+        public Program(List<Item> items)
+        {
+            Items = items;
+        }
+        public Program() { }
+
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
@@ -36,80 +43,84 @@ namespace GildedRose.Console
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (Item item in Items)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                ProcessItem(item);
+            }
+        }
+
+        public static void ProcessItem(Item item)
+        {
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.Quality > 0)
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                 }
-                else
+            }
+            else
+            {
+                if (item.Quality < 50)
                 {
-                    if (Items[i].Quality < 50)
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 11)
                         {
-                            if (Items[i].SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
                         }
-                    }
-                }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 6)
                         {
-                            if (Items[i].Quality > 0)
+                            if (item.Quality < 50)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
                         }
                     }
                 }
             }
-        }
 
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
+                {
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.Quality > 0)
+                        {
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                item.Quality = item.Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+        }
     }
 
     public class Item
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,3 +1,6 @@
+using FluentAssertions;
+using GildedRose.Console;
+using System.Collections.Generic;
 using Xunit;
 
 namespace GildedRose.Tests
@@ -9,5 +12,64 @@ namespace GildedRose.Tests
         {
             Assert.True(true);
         }
+        [Fact]
+        public void ExistingProgramShouldRunAsPrescribed()
+        {
+            var items = new List<Item>
+                                          {
+                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                                              new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15,Quality = 20},
+                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                                          };
+
+            Program p = new Program(items);
+
+            p.UpdateQuality();
+            items[0].SellIn.Should().Be(9);
+            items[1].SellIn.Should().Be(1);
+            items[2].SellIn.Should().Be(4);
+            items[3].SellIn.Should().Be(0);
+            items[4].SellIn.Should().Be(14);
+            items[5].SellIn.Should().Be(2);
+            
+            items[0].Quality.Should().Be(19);
+            items[1].Quality.Should().Be(1);
+            items[2].Quality.Should().Be(6);
+            items[3].Quality.Should().Be(80);
+            items[4].Quality.Should().Be(21);
+            items[5].Quality.Should().Be(5);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void ProcessItemShouldUpdateAsPrescribed(string name, int sellin, int quality, int newSellin, int newQuality) {
+            var item = new Item { Name = name, Quality = quality, SellIn = sellin}; ;
+            Program.ProcessItem(item);
+            item.Quality.Should().Be(newQuality);
+            item.SellIn.Should().Be(newSellin);
+        }
+
+        public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[] { VEST, 10, 20,9,19 }, 
+            new object[] { BRIE, 2,0,1,1 },
+            new object[] { ELIXIR, 5,7,4,6 }, 
+            new object[] {SULFURAS, 0,80,0,80},
+            new object[] { PASSES, 15,20,14,21 },
+            new object[] {CONJURED, 3, 6, 2,5 },
+        };
+
+
+         public const string VEST = "+5 Dexterity Vest";
+         public const string BRIE = "Aged Brie";
+         public const string ELIXIR = "Elixir of the Mongoose";
+         public const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+         public const string PASSES = "Backstage passes to a TAFKAL80ETC concert";
+         public const string CONJURED = "Conjured Mana Cake";
     }
 }
